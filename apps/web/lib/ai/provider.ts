f8521e732ad083c7085c7b7ab2d9095942781ed5
Sanjin <3265832331@qq.com>
2026-02-10 15:37:10 +0800

    add devtools

--- apps/web/lib/ai/provider.ts
+++ apps/web/lib/ai/provider.ts
@@ -1,9 +1,20 @@
-import { gateway } from "ai"
+import { devToolsMiddleware } from "@ai-sdk/devtools"
+import { gateway, wrapLanguageModel } from "ai"
 import { chatModels, DEFAULT_CHAT_MODEL } from "./models"
 
 export function getChatModel(modelId?: string) {
   const selectedId = modelId ?? DEFAULT_CHAT_MODEL
   const isValid = chatModels.some((m) => m.id === selectedId)
+
+  //  dev 启动 ai sdk  vercel  devtools
+  if (process.env.NODE_ENV === "development") {
+    const model = wrapLanguageModel({
+      model: gateway(isValid ? selectedId : DEFAULT_CHAT_MODEL),
+      middleware: devToolsMiddleware(),
+    })
+    return model
+  }
+
   return gateway(isValid ? selectedId : DEFAULT_CHAT_MODEL)
 }
 
